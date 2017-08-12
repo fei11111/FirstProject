6cf13c8d0eec076c5f673d073bcc32011dbeebee
fei <455842151@qq.com>
2017-08-12 17:02:50 +0800

    2017-8-12

--- app/src/main/java/com/fei/firstproject/inter/BaseInterface.java
+++ app/src/main/java/com/fei/firstproject/inter/BaseInterface.java
@@ -11,22 +11,49 @@ import android.view.View;
 
 public interface BaseInterface {
 
+    /**
+     * 初始化前请求权限
+     */
     public void requestPermissionsBeforeInit();
 
+    /**
+     * 权限被拒返回
+     */
     public void permissionsDeniedCallBack(int requestCode);
 
+    /**
+     * 授权后返回
+     */
     public void permissionsGrantCallBack(int requestCode);
 
+    /**
+     * 返回布局
+     */
     public int getContentViewResId();
 
+    /**
+     * 初始化
+     */
     public void init(Bundle savedInstanceState);
 
+    /**
+     * dialog提示被拒返回
+     */
     public void permissionDialogDismiss(int requestCode);
 
+    /**
+     * 没有请求码启动Activity
+     */
     public void startActivityWithoutCode(Intent intent);
 
+    /**
+     * 有请求码启动Activity
+     */
     public void startActivityWithCode(Intent intent, int requestCode);
 
+    /**
+     * 有请求码和共享元素启动Activity
+     */
     public void startActivityWithCodeAndPair(Intent intent, int requestCode, Pair<View, String>... sharedElements);
 
 }
