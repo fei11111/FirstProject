50701b746a501503932c264112e59c7dfddffc69
fei <455842151@qq.com>
2017-08-17 11:20:31 +0800

    2017-8-12

--- app/src/main/java/com/fei/firstproject/zxing/camera/CameraConfigurationManager.java
+++ app/src/main/java/com/fei/firstproject/zxing/camera/CameraConfigurationManager.java
@@ -24,6 +24,7 @@ import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 final class CameraConfigurationManager {
@@ -68,14 +69,33 @@ final class CameraConfigurationManager {
    * and the planar Y can be used for barcode scanning without a copy in some cases.
    */
   void setDesiredCameraParameters(Camera camera) {
+//    Camera.Parameters parameters = camera.getParameters();
+//    Log.d(TAG, "Setting preview size: " + cameraResolution);
+//    parameters.setPreviewSize(cameraResolution.x, cameraResolution.y);
+//    setFlash(parameters);
+//    setZoom(parameters);
+//    //setSharpness(parameters);
+//    //modify here
+//    camera.setDisplayOrientation(90);
+//    camera.setParameters(parameters);
     Camera.Parameters parameters = camera.getParameters();
+    List<Camera.Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
+    int position =0;
+    if(supportedPreviewSizes.size()>2){
+      position=supportedPreviewSizes.size()/2+1;//supportedPreviewSizes.get();
+    }else {
+      position=supportedPreviewSizes.size()/2;
+    }
+
+    int width = supportedPreviewSizes.get(position).width;
+    int height = supportedPreviewSizes.get(position).height;
     Log.d(TAG, "Setting preview size: " + cameraResolution);
-    parameters.setPreviewSize(cameraResolution.x, cameraResolution.y);
+    camera.setDisplayOrientation(90);
+    cameraResolution.x=width;
+    cameraResolution.y=height;
+    parameters.setPreviewSize(width,height);
     setFlash(parameters);
     setZoom(parameters);
-    //setSharpness(parameters);
-    //modify here
-    camera.setDisplayOrientation(90);
     camera.setParameters(parameters);
   }
 
