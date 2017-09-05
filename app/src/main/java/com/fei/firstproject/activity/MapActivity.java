f8fe8d36cd29e6b0b969cd0384db1232a88eb962
fei <455842151@qq.com>
2017-09-05 18:52:53 +0800

    2017-9-5

--- app/src/main/java/com/fei/firstproject/activity/MapActivity.java
+++ app/src/main/java/com/fei/firstproject/activity/MapActivity.java
@@ -1,6 +1,5 @@
 package com.fei.firstproject.activity;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +8,6 @@ import android.widget.Button;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
-import com.amap.api.maps.model.BitmapDescriptor;
-import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.fei.firstproject.R;
 
@@ -81,15 +78,15 @@ public class MapActivity extends BaseActivity {
         aMap = mMapView.getMap();
         UiSettings uiSettings = aMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(false);
+        uiSettings.setMyLocationButtonEnabled(true);
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//定位一次，且将视角移动到地图中心点。
+        myLocationStyle.showMyLocation(true);
+        myLocationStyle.strokeColor(R.color.colorBlue);
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_location));
-        myLocationStyle.myLocationIcon(bitmapDescriptor);
     }
 
     @Override
