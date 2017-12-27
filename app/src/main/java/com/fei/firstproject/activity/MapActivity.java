d83b50445ebae2b6f55b78c949cfdf885e33839f
fei <455842151@qq.com>
2017-12-27 18:46:28 +0800

    2017-12-27 修改地图页面

--- app/src/main/java/com/fei/firstproject/activity/MapActivity.java
+++ app/src/main/java/com/fei/firstproject/activity/MapActivity.java
@@ -1,18 +1,21 @@
 package com.fei.firstproject.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.fei.firstproject.R;
 import com.fei.firstproject.utils.LogUtils;
@@ -23,7 +26,7 @@ import butterknife.BindView;
  * Created by Fei on 2017/9/4.
  */
 
-public class MapActivity extends BaseActivity implements AMapLocationListener, LocationSource {
+public class MapActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -37,9 +40,6 @@ public class MapActivity extends BaseActivity implements AMapLocationListener, L
     TextView tvAddress;
 
     private AMap aMap;
-    private AMapLocationClient mlocationClient;
-    private AMapLocationClientOption mLocationOption;
-    private OnLocationChangedListener mListener;
 
     @Override
     protected void onResume() {
@@ -56,9 +56,6 @@ public class MapActivity extends BaseActivity implements AMapLocationListener, L
     @Override
     protected void onDestroy() {
         mMapView.onDestroy();
-        if (null != mlocationClient) {
-            mlocationClient.onDestroy();
-        }
         super.onDestroy();
     }
 
@@ -91,89 +88,8 @@ public class MapActivity extends BaseActivity implements AMapLocationListener, L
     @Override
     public void init(Bundle savedInstanceState) {
         mMapView.onCreate(savedInstanceState);
-        aMap = mMapView.getMap();
-        aMap.setLocationSource(this);
-        MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(1000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
-        myLocationStyle.strokeColor(android.R.color.transparent);//设置定位蓝点精度圆圈的边框颜色的方法。
-        myLocationStyle.radiusFillColor(android.R.color.transparent);//设置定位蓝点精度圆圈的填充颜色的方法。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        aMap.setMyLocationEnabled(true);
-//        initMap();
-//        initListener();
-    }
-
-//    private void initListener() {
-//        aMap.setLocationSource(new LocationSource() {
-//            @Override
-//            public void activate(OnLocationChangedListener onLocationChangedListener) {
-//                mListener = onLocationChangedListener;
-//                //初始化定位
-//                mlocationClient = new AMapLocationClient(MapActivity.this);
-//                //初始化定位参数
-//                mLocationOption = new AMapLocationClientOption();
-//                //设置定位回调监听
-//                mlocationClient.setLocationListener(MapActivity.this);
-//                //设置为高精度定位模式
-//                mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-//                //设置定位参数
-//                mlocationClient.setLocationOption(mLocationOption);
-//                // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-//                // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-//                // 在定位结束后，在合适的生命周期调用onDestroy()方法
-//                // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-//                mlocationClient.startLocation();//启动定位
-//            }
-//
-//            @Override
-//            public void deactivate() {
-//                mListener = null;
-//                if (mlocationClient != null) {
-//                    mlocationClient.stopLocation();
-//                    mlocationClient.onDestroy();
-//                }
-//                mlocationClient = null;
-//            }
-//        });
-//    }
-//
-//    private void initMap() {
-//        aMap = mMapView.getMap();
-//        UiSettings uiSettings = aMap.getUiSettings();
-//        uiSettings.setZoomControlsEnabled(false);
-//        uiSettings.setMyLocationButtonEnabled(true);
-//        uiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_RIGHT);
-//        MyLocationStyle myLocationStyle;
-//        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-//        myLocationStyle.interval(1000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_location);
-//        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
-//        myLocationStyle.myLocationIcon(bitmapDescriptor);
-//        myLocationStyle.strokeColor(android.R.color.transparent);//设置定位蓝点精度圆圈的边框颜色的方法。
-//        myLocationStyle.radiusFillColor(android.R.color.transparent);//设置定位蓝点精度圆圈的填充颜色的方法。
-//        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-//        aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
-////        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-//    }
-
-    /**
-     * 定位成功后回调函数
-     */
-    @Override
-    public void onLocationChanged(AMapLocation amapLocation) {
-        if (mListener != null && amapLocation != null) {
-            if (amapLocation != null
-                    && amapLocation.getErrorCode() == 0) {
-                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
-                tvAddress.setText(amapLocation.getAddress());
-                LogUtils.i("tag", amapLocation.toString());
-            } else {
-                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
-                LogUtils.e("AmapErr", errText);
-            }
-        }
+        initMap();
+        initListener();
     }
 
     @Override
@@ -181,36 +97,32 @@ public class MapActivity extends BaseActivity implements AMapLocationListener, L
 
     }
 
-    @Override
-    public void activate(OnLocationChangedListener onLocationChangedListener) {
-        mListener = onLocationChangedListener;
-        if (mlocationClient == null) {
-            //初始化定位
-            mlocationClient = new AMapLocationClient(this);
-            //初始化定位参数
-            mLocationOption = new AMapLocationClientOption();
-            //设置定位回调监听
-            mlocationClient.setLocationListener(this);
-            //设置为高精度定位模式
-            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-            //设置定位参数
-            mlocationClient.setLocationOption(mLocationOption);
-            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-            // 在定位结束后，在合适的生命周期调用onDestroy()方法
-            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-            mlocationClient.startLocation();//启动定位
-        }
+    private void initListener() {
+        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LogUtils.i("tag", location.toString());
+            }
+        });
     }
 
-    @Override
-    public void deactivate() {
-        mListener = null;
-        if (mlocationClient != null) {
-            mlocationClient.stopLocation();
-            mlocationClient.onDestroy();
-        }
-        mlocationClient = null;
+    private void initMap() {
+        aMap = mMapView.getMap();
+        UiSettings uiSettings = aMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(false);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_RIGHT);
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(1000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_location);
+        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+        myLocationStyle.myLocationIcon(bitmapDescriptor);
+        myLocationStyle.strokeColor(android.R.color.transparent);//设置定位蓝点精度圆圈的边框颜色的方法。
+        myLocationStyle.radiusFillColor(android.R.color.transparent);//设置定位蓝点精度圆圈的填充颜色的方法。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
     }
-
 }
