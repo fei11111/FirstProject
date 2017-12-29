a40f74edb5ae2f8caf4e7e9697fe4bfa1de57590
fei <455842151@qq.com>
2017-12-29 17:42:58 +0800

    2017-12-29

--- app/src/main/java/com/fei/firstproject/activity/MapActivity.java
+++ app/src/main/java/com/fei/firstproject/activity/MapActivity.java
@@ -1,7 +1,5 @@
 package com.fei.firstproject.activity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -14,8 +12,6 @@ import com.amap.api.maps.AMapOptions;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
-import com.amap.api.maps.model.BitmapDescriptor;
-import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.fei.firstproject.R;
 import com.fei.firstproject.utils.LogUtils;
@@ -101,7 +97,10 @@ public class MapActivity extends BaseActivity {
         aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
+                Bundle extras = location.getExtras();
+                String address = extras.getString("Address","");
                 LogUtils.i("tag", location.toString());
+                tvAddress.setText(address);
             }
         });
     }
@@ -111,16 +110,16 @@ public class MapActivity extends BaseActivity {
         UiSettings uiSettings = aMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(false);
         uiSettings.setMyLocationButtonEnabled(true);
-        uiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_RIGHT);
-        MyLocationStyle myLocationStyle;
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        uiSettings.setLogoPosition(AMapOptions.LOGO_MARGIN_RIGHT);
+        MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(1000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_location);
-        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
-        myLocationStyle.myLocationIcon(bitmapDescriptor);
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_location);
+//        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+//        myLocationStyle.myLocationIcon(bitmapDescriptor);
         myLocationStyle.strokeColor(android.R.color.transparent);//设置定位蓝点精度圆圈的边框颜色的方法。
         myLocationStyle.radiusFillColor(android.R.color.transparent);//设置定位蓝点精度圆圈的填充颜色的方法。
+        myLocationStyle.showMyLocation(true);
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
